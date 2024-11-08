ec12bfb998c6ff6f555010f55086f33b09eb75ca
pranj39 <pranjalshrestha336@gmail.com>
2024-11-08 13:30:45 +0545

    Style and Messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
-
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import 'firebase/firestore';
-import {useAuthState, useSignInWithGoogle} from "react-firebase-hooks/auth"
+import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth"
 import "firebase/auth";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { collection, getDocs, query, getFirestore, QuerySnapshot } from "firebase/firestore";
+import { collection, getDocs, query, getFirestore, QuerySnapshot, onSnapshot, orderBy, serverTimestamp, setDoc, doc, addDoc } from "firebase/firestore";
 
 
 const firebaseConfig = {
@@ -39,71 +38,112 @@ const auth = getAuth(app)
 const firestore = getFirestore(app)
 
 function App() {
-  const [user,loading, error] = useAuthState(auth)
+  const [user, loading, error] = useAuthState(auth)
 
   return (
     <>
-      {user?(
+      {user ? (
         <>
-          <ChatRoom/>
+          <SignOut/>
+          <ChatRoom />
         </>
-      ):(
-        <SignIn/>
+      ) : (
+        <SignIn />
       )}
     </>
   )
 }
 
-function SignIn(){
+function SignIn() {
   let [signInWithGoogle, user] = useSignInWithGoogle(auth)
-  signInWithGoogle =()=>{
-      const provider = new GoogleAuthProvider()
-      signInWithPopup(auth, provider)
-      
+  signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth, provider)
+
   }
-  return(
+  return (
     <>
       <button onClick={signInWithGoogle}>SIGN IN</button>
     </>
   )
 }
-function ChatRoom(){
-    const [messages, setMessages] = useState([]);
+function SignOut(){
+  return auth.currentUser && (
+    <button onClick={()=>auth.signOut()}>SIGN OUT</button>
+  )
+}
+function ChatRoom() {
+  const dummy = useRef()
+  const [messages, setMessages] = useState([]);
+  const [formValue, setFormValue] = useState('')
 
-    useEffect(()=>{
-      fetchMessages()
-    },[]);
+  useEffect(() => {
+    fetchMessages()
+  }, []);
+  useEffect(()=>(scrollToView),[])
+
+  const scrollToView = ()=>{
+    console.log(dummy)
+    dummy.current.scrollIntoView({behavior:'smooth', block:'center'})
     
-      
-    const fetchMessages = async ()=>{
+  }
+  const sendMessage = async (e) => {
+    e.preventDefault()
+    setFormValue('')
+    scrollToView()
+    const { uid, photoURL } = auth.currentUser
+    await addDoc(collection(firestore, "messages"), {
+      message: formValue,
+      createdAt: serverTimestamp(),
+      uid
+    })
 
-      const qSnapshot =  await getDocs(collection(firestore, "messages"))
-      let messagess = qSnapshot.docs.map((message)=>
-      ({...message.data(), id:message.id})
+  }
+
+  const fetchMessages = async () => {
+    const collection_ = collection(firestore, "messages")
+    const query_ = query(collection_, orderBy("createdAt"))
+    onSnapshot(query_, async () => {
+      const qSnapshot = await getDocs(query_)
+      let messagess = qSnapshot.docs.map((message) =>
+        ({ ...message.data(), id: message.id })
       )
       setMessages(messagess)
-    
-    }    
-    const messages_array=messages.map((message)=>(
-      message.message)
-    )
-    return(
-        <>
-          <ChatMessages message={messages_array}></ChatMessages>
-        </>
-      )
-    
-  
+
+    })
+
+  }
+  const messages_array = messages.map((message) => (
+    <ChatMessages key={message.id} message={message}></ChatMessages>)
+  )
+  return (
+    <div className="wrapper">
+      <div className="messages">
+        <div className="message-content">
+        {messages_array}
+        <div ref={dummy}></div>
+        </div>
+        <form className="message-input" onSubmit={sendMessage}>
+          <input value={formValue} onChange={(e) => { setFormValue(e.target.value) }}></input>
+          <button type="onSubmit">SEND</button>
+        </form>
+      </div>
+    </div>
+  )
+
+
 }
-function ChatMessages(props){
-  console.log("OK")
+function ChatMessages(props) {
+  const { message, uid } = props.message
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
   return (<>
-    <p className="messages"> 
-    {props.message}
-    </p>
-    </>
-   )
-  
+    <div className={`message ${messageClass}`}>
+      {message}
+    </div>
+    <div className="timestamp"></div>
+  </>
+  )
+
 }
 
 
