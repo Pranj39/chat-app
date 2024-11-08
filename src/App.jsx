0ce622fb613df12c34f354dfef52f6d0157f7540
pranj39 <pranjalshrestha336@gmail.com>
2024-11-08 10:55:55 +0545

    BUG FIXES

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+
+
+import { useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import 'firebase/firestore';
 import {useAuthState, useSignInWithGoogle} from "react-firebase-hooks/auth"
-import 'firebase/auth';
-import { collection, query } from 'firebase/firestore/lite';
+import "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { collection, getDocs, query, getFirestore, QuerySnapshot } from "firebase/firestore";
+
 
 const firebaseConfig = {
 
@@ -31,27 +35,27 @@ const app = initializeApp(firebaseConfig);
 
 const analytics = getAnalytics(app);
 
-auth = getAuth(app)
-firestore = getFirestore(app)
-[user,loading, error] = useAuthState(auth)
+const auth = getAuth(app)
+const firestore = getFirestore(app)
 
 function App() {
+  const [user,loading, error] = useAuthState(auth)
 
   return (
     <>
-      user?(
+      {user?(
         <>
           <ChatRoom/>
         </>
       ):(
         <SignIn/>
-      )
+      )}
     </>
   )
 }
 
 function SignIn(){
-  [signInWithGoogle, user] = useSignInWithGoogle(auth)
+  let [signInWithGoogle, user] = useSignInWithGoogle(auth)
   signInWithGoogle =()=>{
       const provider = new GoogleAuthProvider()
       signInWithPopup(auth, provider)
@@ -59,22 +63,48 @@ function SignIn(){
   }
   return(
     <>
-      <button onClick={signInWithGoogle()}>SIGN IN</button>
+      <button onClick={signInWithGoogle}>SIGN IN</button>
     </>
   )
 }
-
 function ChatRoom(){
-  messagesRef = collection(firestore,'messages')
-  messages = query(messagesRef,)
+    const [messages, setMessages] = useState([]);
+
+    useEffect(()=>{
+      fetchMessages()
+    },[]);
+    
+      
+    const fetchMessages = async ()=>{
+
+      const qSnapshot =  await getDocs(collection(firestore, "messages"))
+      let messagess = qSnapshot.docs.map((message)=>
+      ({...message.data(), id:message.id})
+      )
+      setMessages(messagess)
+    
+    }    
+    const messages_array=messages.map((message)=>(
+      message.message)
+    )
+    return(
+        <>
+          <ChatMessages message={messages_array}></ChatMessages>
+        </>
+      )
+    
   
-
-
-  return (
-    <>
-      <ChatMessages messages={messages}/>
-    </>
-  )
 }
+function ChatMessages(props){
+  console.log("OK")
+  return (<>
+    <p className="messages"> 
+    {props.message}
+    </p>
+    </>
+   )
+  
+}
+
 
 export default App
